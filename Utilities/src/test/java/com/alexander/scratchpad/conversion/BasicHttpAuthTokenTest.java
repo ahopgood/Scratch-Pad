f4264195de5178abb8a157a45a5240119e0c7fc1
Alexander Hopgood <alex.hopgood@gmail.com>
2020-10-18 21:07:05 +0100

    Migrating CyclicXORTest to JUnit5

--- Utilities/src/test/java/com/alexander/scratchpad/conversion/BasicHttpAuthTokenTest.java
+++ Utilities/src/test/java/com/alexander/scratchpad/conversion/BasicHttpAuthTokenTest.java
@@ -2,7 +2,6 @@ package com.alexander.scratchpad.conversion;
 
 import org.junit.jupiter.api.Test;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BasicHttpAuthTokenTest {
